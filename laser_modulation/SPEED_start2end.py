29c616dda781878730049bb85763791a7302bd86
Arjun Radha Krishnan <arjundortmund@gmail.com>
2023-05-17 14:06:36 +0200

    Update SPEED_start2end.py

--- laser_modulation/SPEED_start2end.py
+++ laser_modulation/SPEED_start2end.py
@@ -55,13 +55,13 @@ l2_E= 4e-3
 Delay_SecondPulse = lambda I : (8.57242860e-04* I**2 + -1.17140062e-01* I + 1.61857761e+01)/2   # Calculate the delay for the second seed pulse
 
 
-IC1 = 750                    # Current for the first chicane
+IC1 = 690                    # Current for the first chicane
 delay_z = Delay_SecondPulse(IC1) * 1e-6    # Corresponding R56 value in microns
 
 ## First calculate energy modualtion amplitude and the optimum chicane currents
 ##### defining the magnetic configuration#####
 
-lattice = Lattice(E0= 1500, l1= l1_wl, l2= l2_wl, h=5, c1= IC1 , c2= 600, plot= 1)
+lattice = Lattice(E0= 1500, l1= l1_wl, l2= l2_wl, h=5, c1= IC1 , c2= 635, plot= 1)
 
 l1= Laser(wl=l1_wl,sigx=1*l1_sigx,sigy=l1_sigx/1,pulse_len=l1_fwhm,pulse_E=l1_E,focus=1.125,M2=1,pulsed=1,phi=0e10)
 l2= Laser(wl=l2_wl,sigx=1*l2_sigx,sigy=1*l2_sigx,pulse_len=l2_fwhm,pulse_E=l2_E,focus=3.3125,X0=0.0e-3,Z_offset=delay_z,M2=1,pulsed=1,phi=0e10)
@@ -76,7 +76,7 @@ z,dE=calc_phasespace(elec_test,e_E,plot=True)
 A22=(max(dE))
 print("A1= ",A11,"\t A2= ",A22)
 
-R56_1_opt , R56_2_opt = calc_R56(A11, A22, m=3)
+R56_1_opt , R56_2_opt = calc_R56(A11, A22, m=4)
 
 #%%
 ###### Calculate the curresponding chicane currents and define a new lattice object
@@ -90,23 +90,23 @@ IC2 = max(np.roots([0.203e-3 , -35.5e-3 , -R56_2_opt*1e6 + 4.01]))
 
 delay_z = Delay_SecondPulse(IC1) * 1e-6 
 
-lattice = Lattice(E0= 1500, l1= l1_wl, l2= l2_wl, h=5, c1= IC1 , c2= IC2 , plot= 1)
-l2= Laser(wl=l2_wl,sigx=1*l2_sigx,sigy=1*l2_sigx,pulse_len=l2_fwhm,pulse_E=l2_E,focus=3.3125,X0=0.0e-3,Z_offset=delay_z,M2=1,pulsed=1,phi=0e10)
+lattice = Lattice(E0= 1500, l1= l1_wl, l2= l2_wl, h=5, c1= IC1 , c2= IC2+100 , plot= 1)
+l2 = Laser(wl=l2_wl,sigx=1*l2_sigx,sigy=1*l2_sigx,pulse_len=l2_fwhm,pulse_E=l2_E,focus=3.3125,X0=0.0e-3,Z_offset=delay_z,M2=1,pulsed=1,phi=0e10)
 
-elec_test= lsrmod_track(lattice,l1,bunch_test,tstep=tstep,disp_Progress=False)
-z,dE=calc_phasespace(elec_test,e_E,plot=True)
-A11=(max(dE))
+# elec_test = lsrmod_track(lattice,l1,bunch_test,tstep=tstep,disp_Progress=False)
+# z,dE = calc_phasespace(elec_test,e_E,plot=True)
+# A11 = (max(dE))
 
-elec_test= lsrmod_track(lattice,l2,bunch_test,tstep=tstep,disp_Progress=False)
-z,dE=calc_phasespace(elec_test,e_E,plot=True)
-A22=(max(dE))
-print("A1= ",A11,"\t A2= ",A22)
+# elec_test = lsrmod_track(lattice,l2,bunch_test,tstep=tstep,disp_Progress=False)
+# z , dE = calc_phasespace(elec_test,e_E,plot=True)
+# A22 = (max(dE))
+# print("A1= ", A11 ,"\t A2= ", A22)
 
-R56_1_opt , R56_2_opt = calc_R56(A11, A22, m=3)
+# R56_1_opt , R56_2_opt = calc_R56(A11, A22, m=4)
 
 #%%
 
-elec = define_bunch(E0=e_E,dE=sigma_E,N=1e5,slicelength=slicelength)
+elec = define_bunch(E0=e_E,dE=sigma_E,N=10e4,slicelength=slicelength)
 print("\nTracking through the lattice...")
 elec_M1= lsrmod_track(lattice,l1,elec,Lsr2=l2,tstep=tstep)
 z,dE=calc_phasespace(elec_M1,e_E,plot=True)
@@ -119,11 +119,13 @@ plt.plot(wl,b)
 
 #%%
 ########## Trying out different R56_2 values ###########
+
 elec = define_bunch(E0=e_E,dE=sigma_E,N=1e4,slicelength=slicelength)
-IC2_list = np.linspace(IC2-50,IC2+50,11)
+IC2_list = np.linspace(IC2+50,IC2+100,11)
+wl = np.linspace(20e-9, 220e-9, 1001)
 bmax = []
-for IC2 in IC2_list:
-    lattice = Lattice(E0= 1500, l1= l1_wl, l2= l2_wl, h=5, c1= IC1 , c2= IC2 , plot= 0)
+for C2 in IC2_list:
+    lattice = Lattice(E0= 1500, l1= l1_wl, l2= l2_wl, h=5, c1= IC1 , c2= C2 , plot= 0)
     elec_M1= lsrmod_track(lattice,l1,elec,Lsr2=l2,tstep=tstep)
     z,dE=calc_phasespace(elec_M1,e_E,plot=False)
     b = calc_bn(z,wl)
@@ -132,3 +134,21 @@ for IC2 in IC2_list:
 
 plt.figure()
 plt.plot(IC2_list, bmax)
+
+#%%
+########## Trying out different R56_1 values ###########
+
+elec = define_bunch(E0=e_E,dE=sigma_E,N=1e4,slicelength=slicelength)
+IC1_list = np.linspace(IC1-50,IC1+50,11)
+bmax = []
+for C1 in IC1_list:
+    lattice = Lattice(E0= 1500, l1= l1_wl, l2= l2_wl, h=7, c1= C1 , c2= IC2 , plot= 0)
+    l2= Laser(wl=l2_wl,sigx=1*l2_sigx,sigy=1*l2_sigx,pulse_len=l2_fwhm,pulse_E=l2_E,focus=3.3125,X0=0.0e-3,Z_offset=Delay_SecondPulse(C1)*1e-6 ,M2=1,pulsed=1,phi=0e10)
+    elec_M1= lsrmod_track(lattice,l1,elec,Lsr2=l2,tstep=tstep)
+    z,dE=calc_phasespace(elec_M1,e_E,plot=False)
+    b = calc_bn(z,wl)
+    bmax.append(max(b))                    
+    plt.plot(wl,b)
+
+plt.figure()
+plt.plot(IC1_list, bmax)
