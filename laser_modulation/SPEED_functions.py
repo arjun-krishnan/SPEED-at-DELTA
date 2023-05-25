d1459e5ca474320f7da8d123b5e22019a8fa76a3
Arjun Radha Krishnan <arjundortmund@gmail.com>
2023-05-25 15:33:40 +0200

    reformatted for cleaner look

--- laser_modulation/SPEED_functions.py
+++ laser_modulation/SPEED_functions.py
@@ -28,13 +28,10 @@ Z0 = 376.73                     # impedance of free space in Ohm
 epsilon_0 = const.epsilon_0     # vacuum permittivity
 mu0 = const.mu_0                # vacuum permeability
 
-#e_E=1.492e9*e_charge   # electron energy in J
-#e_gamma=e_E/m_e/c**2 # Lorentz factor
 
 #@jit(parallel = True)
 def calc_bn(tau0, wl, printmax = True):
     bn = []
-    
     for i in range(len(wl)):
         z = np.sum(np.exp(-1j * 2 * np.pi * (tau0 / wl[i])))
         bn.append(abs(z) / len(tau0))
@@ -72,63 +69,63 @@ def plot_slice(z, wl, slice_len=0, n_slice=40):
     
 def write_results(bunch,file_path):
     print("Writing to "+file_path+" ...")
-    file=pathlib.Path(file_path)
+    file = pathlib.Path(file_path)
     if file.is_file():
-        ch=input("The file already exist! Overwrite? (Y/N)")
-        if ch=='y':
+        ch = input("The file already exist! Overwrite? (Y/N)")
+        if ch == 'y':
             bunch.to_csv(file_path)
     else:
         bunch.to_csv(file_path)
         
 
 def define_bunch(Test=False,N=1e4,slicelength=8e-6,E0=1.492e9*e_charge,dE=7e-4,R56_dE=0.0007):
-    N_e=int(N) # number of electrons
+    N_e = int(N) # number of electrons
     
     ##### electron parameter #####
-    e_E=E0   # electron energy in J
-    energyspread= dE 
+    e_E = E0   # electron energy in J
+    energyspread = dE 
        
-    alphaX=8.811383e-01 #1.8348
-    alphaY=8.972460e-01 #0.1999
-    betaX=13.546
-    betaY=13.401
-    emitX= 1.6e-8
-    emitY= 1.6e-9   
-    Dx= 0.0894
-    Dxprime= -4.3065e-9 
+    alphaX = 8.811383e-01 #1.8348
+    alphaY = 8.972460e-01 #0.1999
+    betaX = 13.546
+    betaY = 13.401
+    emitX = 1.6e-8
+    emitY = 1.6e-9   
+    Dx    = 0.0894
+    Dxprime = -4.3065e-9 
     
     if(Test):
-        slicelength=60e-6 
-        N_e=int(1e4)
-        energyspread= 0e-4
-        emitX= 0
-        emitY= 0  
-        Dx= 0
-        Dxprime= 0
+        slicelength = 60e-6 
+        N_e = int(1e4)
+        energyspread = 0e-4
+        emitX = 0
+        emitY = 0  
+        Dx = 0
+        Dxprime = 0
         
-    CS_inv_x=np.abs(np.random.normal(loc=0,scale=emitX*np.sqrt(2*np.pi),size=N_e))
-    CS_inv_y=np.abs(np.random.normal(loc=0,scale=emitY*np.sqrt(2*np.pi),size=N_e))
-    phase_x=np.random.rand(N_e)*2*np.pi
-    phase_y=np.random.rand(N_e)*2*np.pi
+    CS_inv_x = np.abs(np.random.normal(loc=0,scale=emitX*np.sqrt(2*np.pi),size=N_e))
+    CS_inv_y = np.abs(np.random.normal(loc=0,scale=emitY*np.sqrt(2*np.pi),size=N_e))
+    phase_x = np.random.rand(N_e)*2*np.pi
+    phase_y = np.random.rand(N_e)*2*np.pi
     
     # generate random electron parameters according to beam parameters
-    elec0=np.zeros((6,N_e))
-    elec0[4]=(np.random.rand(1,N_e)-0.5)*slicelength#/c   
-    elec0[5]=np.random.normal(loc=0,scale=energyspread,size=N_e)#/e_m/c**2
-    elec0[0]=(np.sqrt(CS_inv_x*betaX)*np.cos(phase_x))+elec0[5,:]*Dx
-    elec0[1]=-(np.sqrt(CS_inv_x/betaX)*(alphaX*np.cos(phase_x)+np.sin(phase_x)))+elec0[5,:]*Dxprime
-    elec0[2]=(np.sqrt(CS_inv_y*betaY)*np.cos(phase_y))
-    elec0[3]=-(np.sqrt(CS_inv_y/betaY)*(alphaY*np.cos(phase_y)+np.sin(phase_y)))
+    elec0 = np.zeros((6, N_e))
+    elec0[4] = (np.random.rand(1, N_e) - 0.5) * slicelength  # / c
+    elec0[5] = np.random.normal(loc=0, scale=energyspread, size=N_e)  # /e_m/c**2
+    elec0[0] = np.sqrt(CS_inv_x * betaX) * np.cos(phase_x) + elec0[5, :] * Dx
+    elec0[1] = -np.sqrt(CS_inv_x / betaX) * (alphaX * np.cos(phase_x) + np.sin(phase_x)) + elec0[5, :] * Dxprime
+    elec0[2] = np.sqrt(CS_inv_y * betaY) * np.cos(phase_y)
+    elec0[3] = -np.sqrt(CS_inv_y / betaY) * (alphaY * np.cos(phase_y) + np.sin(phase_y))
     
     # Adding two particles with only energy difference to calculate the R56
     for i in range(6):
-        if i==5:
-            elec0[i][-2],elec0[i][-1] = 0, R56_dE
+        if i == 5:
+            elec0[i][-2] , elec0[i][-1] = 0, R56_dE
         else:
-            elec0[i][-2],elec0[i][-1] = 0, 0.0
+            elec0[i][-2] , elec0[i][-1] = 0, 0.0
             
     #changing to parameter style: [x,y,z,px,py,pz] in laboratory frame
-    elec=coord_change(elec0,e_E)
+    elec = coord_change(elec0,e_E)
     np.save("e_dist.npy",elec)
     return(elec)
 
